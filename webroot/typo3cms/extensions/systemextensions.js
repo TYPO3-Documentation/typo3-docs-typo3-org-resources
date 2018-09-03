93d940ced76b89429b45e5eba53b5e2f0613fbc1
Martin Bless <martin.bless@mbless.de>
2018-09-03 16:01:36 +0200

    Empty the systemextensions.js list. Will soon be dropped

--- webroot/typo3cms/extensions/systemextensions.js
+++ webroot/typo3cms/extensions/systemextensions.js
@@ -4,24 +4,24 @@
 // merge list 'systemExtensionList' into list 'extensionList'
 
 var systemExtensionList = [
-	{"key":"core","latest":"latest","versions":["7.6","latest"]},
-	{"key":"css_styled_content","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
-	{"key":"dbal","latest":"latest","versions":["1.0.0","6.2","7.6","latest"]},
-	{"key":"documentation","latest":"latest","versions":["6.2","7.6","latest"]},
-	{"key":"felogin","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
-	{"key":"fluid_styled_content","latest":"latest","versions":["7.6","latest"]},
-	{"key":"form","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
-	{"key":"indexed_search","latest":"latest","versions":["6.2","7.6","latest"]},
-	{"key":"linkvalidator","latest":"latest","versions":["6.2","7.6","latest"]},
-	{"key":"openid","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
-	{"key":"recycler","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
-	{"key":"rsaauth","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
-	{"key":"rtehtmlarea","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
-	{"key":"saltedpasswords","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
-	{"key":"scheduler","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
-	{"key":"sys_action","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
-	{"key":"taskcenter","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
-	{"key":"workspaces","latest":"latest","versions":["6.2","7.6","latest"]}
+//	{"key":"core","latest":"latest","versions":["7.6","latest"]},
+//	{"key":"css_styled_content","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+//	{"key":"dbal","latest":"latest","versions":["1.0.0","6.2","7.6","latest"]},
+//	{"key":"documentation","latest":"latest","versions":["6.2","7.6","latest"]},
+//	{"key":"felogin","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+//	{"key":"fluid_styled_content","latest":"latest","versions":["7.6","latest"]},
+//	{"key":"form","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+//	{"key":"indexed_search","latest":"latest","versions":["6.2","7.6","latest"]},
+//	{"key":"linkvalidator","latest":"latest","versions":["6.2","7.6","latest"]},
+//	{"key":"openid","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+//	{"key":"recycler","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+//	{"key":"rsaauth","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+//	{"key":"rtehtmlarea","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+//	{"key":"saltedpasswords","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+//	{"key":"scheduler","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+//	{"key":"sys_action","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+//	{"key":"taskcenter","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+//	{"key":"workspaces","latest":"latest","versions":["6.2","7.6","latest"]}
 ];
 
 // var extensionList needs to exist at this point
